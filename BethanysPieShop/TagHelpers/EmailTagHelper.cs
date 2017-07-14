eabe48c70d04e3612d2676cdf7ad5889c38afd19
Rachel Rodini <rachel.rodini@amtrustgroup.com>
2017-07-14 16:29:44 -0400

    Initial commit

---
+++ BethanysPieShop/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace BethanysPieShop.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string Addreess { get; set; }
+        public string Content { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "mailto:" + Addreess);
+            output.Content.SetContent(Content);
+        }
+    }
+}
